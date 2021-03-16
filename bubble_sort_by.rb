a5e6cb6664e408be1f9e4298f7d891f12b1060e1
marijan <marijan@miave.dev>
2021-03-17 00:22:00 +0100

    Add Bubblesort with Microverse Adjustments

---
+++ bubble_sort_by.rb
@@ -0,0 +1,20 @@
+def bubble_sort_by(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n - 1).times do |i|
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  array
+end
+
+sorted_by_array = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
+end
+
+p sorted_by_array
