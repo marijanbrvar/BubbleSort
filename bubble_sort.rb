c1cfc3cf935d80135bb7e04b1f55bcdb57525889
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-03-17 23:01:25 +0200

    attempt-2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,17 +7,35 @@
 # times (iteration)
 # change the value in array
 
-
 def bubble_sort(array)
-  n = array.length
   i = 0
-  for i in n do
-     (n -1).times 
-     if array[i] > array[i + 1]
-     array[i], array[i + 1] = array[i + 1], array[i]
-     puts i  
+  while i < array.length - 1
+    if array[i] > array[i + 1]
+      array[i], array[i + 1] = array[i + 1], array[i]
+      i = 0
+    else
+      i += 1
+    end
   end
-end
   array
 end
+
+array = [4, 0, 2, 5, 8, 12]
+puts bubble_sort(array)
+
+
+# def bubble_sort(array)
+#   n = array.length
+#   for i in n do
+#      (n -1).times 
+#      if array[i] > array[i + 1]
+#      array[i], array[i + 1] = array[i + 1], array[i]
+     
+#      puts i 
+#   end
+#  end
+#   array
+# end
+
+
 
